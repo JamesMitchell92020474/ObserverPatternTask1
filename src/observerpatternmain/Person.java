6b8a2d7f618fe57b30e97bee5c3d21f1d442be4f
James Mitchell <jamesmitchelldesign1@gmail.com>
2019-09-25 17:17:03 +1200

    Person Final Commit

--- src/observerpatternmain/Person.java
+++ src/observerpatternmain/Person.java
@@ -11,6 +11,29 @@ package observerpatternmain;
  * @author James Mitchell 92020474
  * @date 25/09/2019
  */
-public class Person {
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Person implements Observer{
     
+    // set a backing field
+    private final String personName;	
+ 
+    // ctor
+    public Person(String newName) {
+            personName = newName;
+    }
+ 
+    // mandatory method because implements Observer
+
+    /**
+     *
+     * @param mySubject
+     * @param prodName
+     */
+    @Override
+    public void update(Observable mySubject, Object prodName) {
+            System.out.println("Hello "+ personName +", the " + (String) prodName + " is now in stock.");
+    }
 }
