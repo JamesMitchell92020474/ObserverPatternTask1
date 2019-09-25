a72ec4b6c734c7db6e3b2372cb1d55ced1f38aff
James Mitchell <jamesmitchelldesign1@gmail.com>
2019-09-25 17:17:49 +1200

    Product Final Commit

--- src/observerpatternmain/Product.java
+++ src/observerpatternmain/Product.java
@@ -11,6 +11,49 @@ package observerpatternmain;
  * @author James Mitchell 92020474
  * @date 25/09/2019
  */
-public class Product {
+
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Product extends Observable{
+ 
+    // set some backing fields
+    private final ArrayList<Observer> observers;
+    private final String productName;
+    private int qtyInStock;
     
+    // ctor
+    public Product(String prodName) {
+            super();
+            productName = prodName;
+            // set some defaults
+            qtyInStock = 0;
+            observers = new ArrayList<Observer>(){};
+    }
+ 
+    public void setQty(int newQty) {
+        qtyInStock = newQty;
+ 
+        if(qtyInStock > 0)
+        {
+            setChanged();
+            notifyObservers(this, productName);
+        }
+    }
+ 
+    public void notifyObservers(Observable observable,String prodName) {
+            System.out.println("Notification to all subscribers when a Product becomes available:");
+            observers.forEach((ob) -> {
+                ob.update(observable, prodName);
+        });
+    }
+ 
+    public void registerObserver(Observer observer) {
+             observers.add(observer);
+    }
+ 
+    public void removeObserver(Observer observer) {
+             observers.remove(observer);
+    }
 }
