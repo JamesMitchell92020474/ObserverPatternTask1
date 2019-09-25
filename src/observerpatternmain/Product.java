17d0ab48b3cde17dce0fbabaced313b009c06232
James Mitchell <jamesmitchelldesign1@gmail.com>
2019-09-25 17:03:29 +1200

    Product Initial Commit

---
+++ src/observerpatternmain/Product.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package observerpatternmain;
+
+/**
+ *
+ * @title Product.java
+ * @author James Mitchell 92020474
+ * @date 25/09/2019
+ */
+public class Product {
+    
+}
